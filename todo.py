4a5deda0ed0d21bd2f137de1dd616e7673ca82ac
4mirhoseinhasani <abaiat920@gmail.com>
2025-05-09 16:09:39 +0330

    'remove_task' function updated and bug fixed

--- todo.py
+++ todo.py
@@ -9,6 +9,7 @@ class Task:
         self.description = description
         self.priority = priority
     
+
     def __str__(self):
         return f"{self.name} | description: {self.description} | priority: {self.priority} "
 
@@ -16,14 +17,33 @@ class Task:
 class ToDoList:
 
     def __init__(self):        
-        self.tasks = []
+        self.tasks = []    # a list fo save tasks into it
 
 
     def add_task(self):
         print("Add a new Task")
-        name = input("Enter Task name: ")
+        # check  if name is empty
+        while True:
+            name = input("Enter Task name: ")
+            if not name:
+                print("name can not be empty!")
+            else:
+                break
+
         description = input("Enter your task description: ")
-        priority = input("Enter priority (High / Medium / Low): ")
+        if not description:    # default value for description
+            description = "---"
+        
+        # priority validation
+        valid_priorities = ['high', 'medium', 'low']
+        while True:
+            priority = input("Enter priority (High / Medium / Low): ").strip().lower()
+            if priority not in valid_priorities:
+                print("Invalid priority! please type high, medium or low: ")
+            else:
+                priority = priority.capitalize()
+                break
+
         task = Task(name, description, priority)    # make a list of name, description, priority as a task
         self.tasks.append(task)
         print("Task added successfully")
@@ -33,23 +53,26 @@ class ToDoList:
         if not self.tasks:
             print("ToDoList is empty!")
             return
-        
-        # use tabulate for better display tasks
-        table = [[i+1, t.name, t.description, t.priority] for i, t in enumerate(self.tasks)]
-        headers = ["#", "name", "description", "priority"]
-        print(tabulate(table, headers=headers, tablefmt="grid"))
+        # validity check of task number for remove
+        while True:
+            # use tabulate for better display tasks
+            table = [[i+1, t.name, t.description, t.priority] for i, t in enumerate(self.tasks)]
+            headers = ["#", "name", "description", "priority"]
+            print(tabulate(table, headers=headers, tablefmt="grid"))
             
-        try:
-            choice = int(input("Enter the number of the task to remove: "))    
+            try:
+                choice = int(input("Enter the number of the task to remove: "))
+            except ValueError:
+                print("Please Enter valid number.")
+                continue
+
             if 1 <= choice <= len(self.tasks):
                 removed = self.tasks.pop(choice - 1)
-                print(f"removed: {removed.name}\n")    
+                print(f"removed: {removed.name}\n")
+                break                
             else:
-                print("Invalid task number.")
-            return
-        
-        except ValueError:
-            print("Please Enter valid number.")
+                print(f"Invalid task number! please enter a number between 1 to {len(self.tasks)}")
+
     
     
     def show_tasks(self):
@@ -77,6 +100,7 @@ class ToDoList:
             print(f"file {filename} not found!")
             return
 
+
     def save_tasks_to_file(self, filename="result.csv"):
         try:
             with open(filename, mode='w', newline='') as f_out:
@@ -97,6 +121,7 @@ def show_menu():
     print(tabulate(table, headers=headers, tablefmt='grid'))    # print in tabulate form
     choice = input("choose between 1 and 6: ")
     return choice
+
 
 # user interface
 
